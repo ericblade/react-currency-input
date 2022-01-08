b0797fc6a2997ca9e1f08b772f2b9cc246d46bbd
Eric Blade <blade.eric@gmail.com>
2022-01-08 17:08:38 -0500

    fix #4 remove built libs from source
    you will need to npm install this lib

--- dist/esm/mask.d.ts
+++
@@ -1,4 +0,0 @@
-export default function mask(value?: string | number | undefined | null, precision?: string | number, decimalSeparator?: string, thousandSeparator?: string, allowNegative?: boolean, prefix?: string, suffix?: string): {
-    value: number;
-    maskedValue: string;
-};
